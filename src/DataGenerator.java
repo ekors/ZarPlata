b14fe3c0d2abc232e9410f65934fecc4e1fe51ba
ekors <2712ekors@gmail.com>
2017-01-20 18:23:16 +0200

    dataGenerator

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -1,10 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
  * Created by Ievgen.Korsun on 1/20/2017.
  */
 public class DataGenerator {
-
+    public void generateListOfEmployees () {
+        ArrayList<Employee> employeesList = new ArrayList<>();
+        for (int i = 0; i < getRandomNumberInRange(10, 30); i++) {
+            employeesList.add(new Employee(i + 1, "Employee " + (i + 1), getRandomNumberInRange(1950, 2000), getRandomNumberInRange(1000, 5000)));
+        }
+    }
 
     public static int getRandomNumberInRange(int min, int max) {
         if (min >= max) {
